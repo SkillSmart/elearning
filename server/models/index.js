1b9c7e052500d3a89a735595323ac7393e815f73
Frank Fichtenmueller <frank.fichtenmueller@outlook.com>
2018-01-19 01:36:19 +0530

    Added graphql mutations to basic content Schemata

---
+++ server/models/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // Models
+    Course: require('./Course.model'),
+    Module: require('./Module.model'),
+    Lesson: require('./Lesson.model'),
+    Document: require('./Document.model'),
+    Video: require('./Video.model'),
+    Rating: require('./Rating.model'),
+    User: require('./User.model'),
+
+    // Schemata
+    NoteSchema: require('./note.schema')
+};
