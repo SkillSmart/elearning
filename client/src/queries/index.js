e4c596241f9d7593a91d2e866c51cbbe26b3cc1c
Frank Fichtenmueller <frank.fichtenmueller@outlook.com>
2018-01-19 12:31:35 +0530

    added basic course queries to client

---
+++ client/src/queries/index.js
@@ -0,0 +1,9 @@
+export default {
+    user: import('./user.queries.js'),
+    course: import('./course.queries.js'),
+    module: import('./module.queries.js'),
+    lesson: import('./lesson.queries.js'),
+    video: import('./video.queries.js'),
+    document: import('./document.queries.js'),
+    quiz: import('./quiz.queries.js'),
+}
