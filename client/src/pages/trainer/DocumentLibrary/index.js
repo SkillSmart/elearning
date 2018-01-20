318998b14027a62dc7d072af7c9854fa7c89fc6d
Frank Fichtenmueller <frank.fichtenmueller@outlook.com>
2018-01-20 14:39:50 +0530

    added basic Routing and Layout to subpages TrainerQuizLibrary

--- client/src/pages/trainer/DocumentLibrary/index.js
+++ client/src/pages/trainer/DocumentLibrary/index.js
@@ -2,136 +2,95 @@ import React, {Component} from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 
-
 import {LibrarySearchHeader, CourseLibrarySubNavigation} from '../../../components';
-import {LessonQuickAddForm} from '../../../forms';
+import {CourseQuickAddForm} from '../../../forms';
 
+// Routing
+import {Route} from 'react-router-dom';
+import TrainerDocumentCreator from './Trainer-DocumentCreator';
+import TrainerDocumentDashboard from './Trainer-DocumentDashboard';
+import TrainerDocumentManager from './Trainer-DocumentManager';
+import TrainerDocumentLibrary from './Trainer-DocumentLibrary';
 
-class _TrainerDocumentLibrary extends Component {
+const _TrainerDocumentLibrary = ({className, match}) => (
+    <div className={className}>
 
-
-    render() {
-        let {className, match} = this.props;
-        return (
-            <div className={className}>
-
-                {/* Search and Filter Bar */}
-                <LibrarySearchHeader className="searchbar"/>
-
-                <CourseLibrarySubNavigation
-                    baseUrl={match.path}
-                    linkMap={linkMap}
-                    className="subnavigation"/> 
-
-                {/* List of all lessons */}
-                <section className="lesson_table">
-                    <table>
-                        <thead>
-                            <tr>
-                                <td>Title</td>
-                                <td>Headline</td>
-                                <td>Tags</td>
-                                <td>createdAt</td>
-                                <td>assigned</td>
-                                <td>video</td>
-                                <td>documents</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this
-                                .state
-                                .lessons
-                                .map(lesson => (
-                                    <tr>
-                                        <td>{lesson.title}</td>
-                                        <td>{lesson.headline}</td>
-                                        <td>{lesson.tags}</td>
-                                        <td>{lesson.createdAt}</td>
-                                        <td>{lesson.assigned}</td>
-                                        <td>{lesson.video}</td>
-                                        <td>{lesson.documents}</td>
-                                    </tr>
-                                ))}
-                        </tbody>
-                    </table>
-                </section>
-
-                {/* The Sidebar*/}
-                <div className="sidebar">
-                <h3>Quick Add Lesson</h3>
-                    <LessonQuickAddForm />
-                <h3>View Lessons by Course</h3>
-
-                </div>
-            </div>
-        )
-    }
-}
+        <CourseLibrarySubNavigation
+            baseUrl={match.path}
+            linkMap={linkMap}
+            className="subnavigation"/> 
+        
+        {/* Routing */}
+        <main className="display">
+            <Route
+                path={`${match.path}/dashboard`}
+                component={TrainerDocumentDashboard}/>
+            <Route
+                path={`${match.path}/library`}
+                component={TrainerDocumentLibrary}/>
+            <Route
+                path={`${match.path}/manager`}
+                component={TrainerDocumentManager}/>
+            <Route 
+                path={`${match.path}/new`} 
+                component={TrainerDocumentCreator}/>
+        </main>
+        <footer className="footer">
+            The Document Library Footer
+        </footer>
+    </div>
+);
 
 export default styled(_TrainerDocumentLibrary)`
     display: grid;
-    grid-template-columns: 1fr minmax(15rem, .3fr) 1fr 1fr;
-    grid-template-rows: minmax(10rem, .3fr) 1fr 2fr;
-    grid-column-gap: 2rem;
-    grid-row-gap: 3.5rem;
-    padding: 3.5rem;
-    height: 95vh;
-
     grid-template-areas: 
-        "search search search search"
-        "side table table table"
-        ". . . . ";
+        "nav nav nav nav"
+        "sub sub sub sub"
+        "foot foot foot foot";
+       
 
     background: linear-gradient(
         to bottom right,
         lightblue, darkblue
         );
 
-    .searchbar {
-        grid-area: search;
-    }
-
-    .lesson_table {
-        grid-area: table;
+    .subnavigation {
+        grid-area: nav;
         background: white;
-        padding: 2rem;
-        border-radius: 3px;
+        align-self: center;
     }
 
-    .sidebar {
-        grid-area: side;
-        border-radius: 3px;
-        padding: 1.5rem 3.5rem;
-        background: white;
-
+    .display {
+        grid-area: sub;
     }
-
+    
+    .footer {
+        grid-area: foot;
+    }
 
 `;
 
+
+// Additional configuration
 const linkMap = [
     {
-        url: '/dashboard',
+        url: 'dashboard',
         label: "Dashboard"
     },
     {
-        url: '/dashboard',
-        label: "Dashboard"
-    },
-    {
-        url: '/manager',
+        url: 'manager',
         label: "Manager"
     },
     {
-        url: '/library',
-        label: "Library"
+        url: 'library',
+        label: "Document Library"
     },
     {
-        url: '/new',
+        url: 'new',
         label: "Add Document"
     },
     {
-        url: '/close',
-        label: "Retire Course"
+        url: 'close',
+        label: "Retire Document"
     },
 ]
--- client/src/pages/trainer/QuizLibrary/index.js
+++ client/src/pages/trainer/QuizLibrary/index.js
@@ -2,118 +2,91 @@ import React, {Component} from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 
+import {LibrarySearchHeader, CourseLibrarySubNavigation} from '../../../components';
+import {CourseQuickAddForm} from '../../../forms';
 
-import {LibrarySearchHeader} from '../../../components';
-import {LessonQuickAddForm} from '../../../forms';
+// Routing
+import {Route} from 'react-router-dom';
+import TrainerQuizCreator from './Trainer-QuizCreator';
+import TrainerQuizDashboard from './Trainer-QuizDashboard';
+import TrainerQuizManager from './Trainer-QuizManager';
+import TrainerQuizLibrary from './Trainer-QuizLibrary';
 
+const _TrainerQuizLibrary = ({className, match}) => (
+    <div className={className}>
 
-class _TrainerLessonLibrary extends Component {
+        <CourseLibrarySubNavigation
+            baseUrl={match.path}
+            linkMap={linkMap}
+            className="subnavigation"/> 
+        
+        {/* Routing */}
+        <main className="display">
+            <Route
+                path={`${match.path}/dashboard`}
+                component={TrainerQuizDashboard}/>
+            <Route
+                path={`${match.path}/library`}
+                component={TrainerQuizLibrary}/>
+            <Route
+                path={`${match.path}/manager`}
+                component={TrainerQuizManager}/>
+            <Route 
+                path={`${match.path}/new`} 
+                component={TrainerQuizCreator}/>
+        </main>
+        <footer className="footer">
+            The Course Library Footer
+        </footer>
+    </div>
+);
 
-    state = {
-        lessons: []
-    }
-
-    componentWillMount = async() => {
-        try {
-            let response = await axios.get('/lessons');
-            let lessons = response.data;
-            this.setState({lessons});
-        } catch (e) {
-            throw e;
-        }
-
-    }
-
-    render() {
-        let {className} = this.props;
-        return (
-            <div className={className}>
-
-                {/* Search and Filter Bar */}
-                <LibrarySearchHeader className="searchbar"/>
-
-                {/* List of all lessons */}
-                <section className="lesson_table">
-                    <table>
-                        <thead>
-                            <tr>
-                                <td>Title</td>
-                                <td>Headline</td>
-                                <td>Tags</td>
-                                <td>createdAt</td>
-                                <td>assigned</td>
-                                <td>video</td>
-                                <td>documents</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this
-                                .state
-                                .lessons
-                                .map(lesson => (
-                                    <tr>
-                                        <td>{lesson.title}</td>
-                                        <td>{lesson.headline}</td>
-                                        <td>{lesson.tags}</td>
-                                        <td>{lesson.createdAt}</td>
-                                        <td>{lesson.assigned}</td>
-                                        <td>{lesson.video}</td>
-                                        <td>{lesson.documents}</td>
-                                    </tr>
-                                ))}
-                        </tbody>
-                    </table>
-                </section>
-
-                {/* The Sidebar*/}
-                <div className="sidebar">
-                <h3>Quick Add Lesson</h3>
-                    <LessonQuickAddForm />
-                <h3>View Lessons by Course</h3>
-
-                </div>
-            </div>
-        )
-    }
-}
-
-export default styled(_TrainerLessonLibrary)`
+export default styled(_TrainerQuizLibrary)`
     display: grid;
-    grid-template-columns: 1fr minmax(15rem, .3fr) 1fr 1fr;
-    grid-template-rows: minmax(10rem, .3fr) 1fr 2fr;
-    grid-column-gap: 2rem;
-    grid-row-gap: 3.5rem;
-    padding: 3.5rem;
-    height: 95vh;
-
     grid-template-areas: 
-        "search search search search"
-        "side table table table"
-        ". . . . ";
+        "nav nav nav nav"
+        "sub sub sub sub"
+        "foot foot foot foot";
+       
 
     background: linear-gradient(
         to bottom right,
         lightblue, darkblue
         );
 
-    .searchbar {
-        grid-area: search;
-    }
-
-    .lesson_table {
-        grid-area: table;
+    .subnavigation {
+        grid-area: nav;
         background: white;
-        padding: 2rem;
-        border-radius: 3px;
+        align-self: center;
     }
 
-    .sidebar {
-        grid-area: side;
-        border-radius: 3px;
-        padding: 1.5rem 3.5rem;
-        background: white;
-
+    .display {
+        grid-area: sub;
     }
-
+    
+    .footer {
+        grid-area: foot;
+    }
 
 `;
+
+
+// Additional configuration
+const linkMap = [
+    {
+        url: 'dashboard',
+        label: "Dashboard"
+    },
+    {
+        url: 'manager',
+        label: "Quiz Manager"
+    },
+    {
+        url: 'library',
+        label: "Quiz Library"
+    },
+    {
+        url: 'new',
+        label: "Add Quiz"
+    },
+]
